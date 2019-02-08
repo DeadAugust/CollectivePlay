a2ca79fdc4172222ccbd1e70bc5186a8386b4a9f
DeadAugust <augustluhrs@gmail.com>
2019-02-08 15:17:19 -0500

    base template for dBattle

---
+++ dBattle/public/screen/sketch.js
@@ -0,0 +1,23 @@
+// Open and connect socket
+let socket = io();
+
+function setup(){
+  //- - - - - overall
+	// var screenSize = windowHeight - 100;
+	// createCanvas(int(screenSize * .666), screenSize);
+	createCanvas(windowWidth, windowHeight);
+	background(0, 150, 50);
+
+
+  // Listen for confirmation of connection
+  socket.on('connect', function() {
+    console.log("Connected");
+  });
+
+	// - - - - - heartbeat
+	socket.on('heartbeat',
+		function(data){
+
+		}
+	);
+}
